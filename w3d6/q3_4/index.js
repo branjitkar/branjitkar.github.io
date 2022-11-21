32c3b15be785225ab6a557b653931550b52ac865
Bipul Ranjitkar <branjitkar@users.noreply.github.com>
2022-11-21 10:41:07 -0600

    w3d6 style updates

--- w3d6/q3_4/index.js
+++ w3d6/q3_4/index.js
@@ -3,6 +3,31 @@ const path = require("path");
 const app = express();
 const productRouter = require("./route/product");
 
+const products = [
+  { name: "Apple", price: 20, desc: "Fresh appples", id: 1 },
+  { name: "Orange", price: 15, desc: "Fresh oranges", id: 2 },
+  { name: "Banana", price: 40, desc: "Raw bananas", id: 3 },
+  { name: "Pear", price: 50, desc: "Just Pears", id: 4 },
+];
+const cart = [
+  {
+    id: 1,
+    name: "Apple",
+    price: 20,
+    desc: "Fresh appples",
+    prodId: 1,
+    quantity: 1,
+  },
+  {
+    id: 2,
+    name: "Orange",
+    price: 15,
+    desc: "Fresh oranges",
+    prodId: 2,
+    quantity: 1,
+  },
+];
+
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -12,7 +37,7 @@ app.use("/style", express.static(path.join(__dirname, "style")));
 app.use("/product", productRouter);
 
 app.get("/", (req, res) => {
-  res.redirect("/product");
+  res.render(path.join(__dirname, "view", "main.ejs"), { products, cart });
 });
 
 app.listen(3000, () => {
--- w3d6/q3_4/route/product.js
+++ w3d6/q3_4/route/product.js
@@ -1,18 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const products = [
-  { name: "Apple", price: 20, desc: "Fresh appples", id: 1 },
-  { name: "Orange", price: 15, desc: "Fresh oranges", id: 2 },
-  { name: "Banana", price: 40, desc: "Raw bananas", id: 3 },
-  { name: "Pear", price: 50, desc: "Just Pears", id: 4 },
-];
-const cart = [
-  { name: "Apple", price: 20, desc: "Fresh appples", id: 1, quantity: 1 },
-  { name: "Orange", price: 15, desc: "Fresh oranges", id: 2, quantity: 1 },
-];
-router.get("/", (req, res) => {
-  res.render("../view/product.ejs", products[0]);
-});
+
 router.get("/cart", (req, res) => {
   res.render("../view/shoppingcart.ejs", { cart });
 });
